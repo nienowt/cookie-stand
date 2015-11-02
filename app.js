ef152a1af3c1a7d764f87b230280e08f5ea03244
Thom Nienow <elstad.nienow@gmail.com>
2015-11-02 15:58:32 -0800

    added function to display totals in html

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var pikePlace = {
   avgPurch: 5.2,
   hourTotal: [],
   dailyTotal: 0,
-
+  hour: ["10:00am: ","11:00am: ","12:00pm: ","1:00pm: ","2:00pm: ","3:00pm: ","4:00pm: ","5:00pm: "],
 
   generateRandom: function(min, max) {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
@@ -12,27 +12,40 @@ var pikePlace = {
 
   calcHourly: function() {
     for (var i=0; i<8; i++) {
-    var hourly = this.generateRandom() * this.avgPurch;
+    var hourly = Math.floor(this.generateRandom() * this.avgPurch);
       this.hourTotal.push(hourly);
     }
-    console.log(this.hourTotal);
   },
 
   calcDaily: function(){
     for (var i=0; i<8; i++) {
       this.dailyTotal += this.hourTotal[i];
     }
-    console.log(this.dailyTotal);
+  },
+
+  displayTotals: function() {
+  for (var i=0; i<8;i++) {
+    var results = document.createElement('ul');
+    var timeTotal = document.createElement('li');
+
+    timeTotal.innerHTML = this.hour[i] + this.hourTotal[i];
+    document.body.appendChild(timeTotal);
+    }
+
+  var finalTotal = document.createElement('li')
+  finalTotal.innerHTML = "Total: " + this.dailyTotal;
+  document.body.appendChild(finalTotal);
+
   }
 };
 
-// function displayTotals() {
-//   for (var i=0; i<pikePlace.hourTotal.length;i++) {
-//     var time = document.createElement('ul');
-//     var timetotal = document.createElement('li');
-//   }
+pikePlace.calcHourly();
+pikePlace.calcDaily();
+pikePlace.displayTotals();
 
-// }
+
+
+
 
 
 
