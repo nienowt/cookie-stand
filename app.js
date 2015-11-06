1b9f76bfd9c090caa18d8f16748d91afc8eae27c
Thom Nienow <elstad.nienow@gmail.com>
2015-11-05 20:12:18 -0800

    added new html page, css

--- app.js
+++ app.js
@@ -12,15 +12,15 @@ function CookieStand(loc, minCust, maxCust, avgPurch) {
 
   this.generateRandom = function(min, max) {
     return Math.floor(Math.random() * (this.maxCust - this.minCust + 1)) + this.minCust;
-  };
+  }
 
   this.calcHourly = function() {
-    for (var i=0; i<hour.length-2; i++) {  //-2 accounts for extra values in hour array, empty cell and total
+    for (var i = 0; i<hour.length-2; i++) {  //-2 accounts for extra values in hour array, empty cell and total
     var hourly = Math.floor(this.generateRandom() * this.avgPurch);
       this.hourTotal.push(hourly);
       this.dailyTotal += this.hourTotal[i];
     }
-  };
+  }
 };
 
 var pike = new CookieStand('Pike Place', 17,88,5.2);
@@ -30,16 +30,16 @@ var bellSquare = new CookieStand('Bellevue Square', 20,48,3.3);
 var alki = new CookieStand('Alki', 3,24,2.6);
 //-----table-------
 function displayTable() {
-  var tbl = document.createElement('table');
+  var tbl = document.createElement('table');  //move these variables outside into function outside of displayTable
   tbl.id='tbl';
   var headerRow = document.createElement('tr');
 
-    for (i = 0; i < hour.length; i++) {
-      var thElement = document.createElement('th');
-      thElement.textContent = hour[i];
-      headerRow.appendChild(thElement);
-    }
-     tbl.appendChild(headerRow);
+  for (i = 0; i < hour.length; i++) {              //break up into different functions
+    var thElement = document.createElement('th');
+    thElement.textContent = hour[i];
+    headerRow.appendChild(thElement);
+  }
+    tbl.appendChild(headerRow);
 
   for (i = 0; i < allCookieStands.length; i++) {
     allCookieStands[i].calcHourly();
@@ -48,13 +48,11 @@ function displayTable() {
     tdElement.textContent = allCookieStands[i].loc;
     trElement.appendChild(tdElement);
     tbl.appendChild(trElement);
-
-      for  (j = 0; j < hour.length-2; j++) {
-      var tdElement2 = document.createElement('td');
+    for  (j = 0; j < hour.length-2; j++) {
+      var tdElement2 = document.createElement('td');           //move into object to add line rather than refresh table
       tdElement2.textContent = allCookieStands[i].hourTotal[j];
       trElement.appendChild(tdElement2);
     }
-
     var resultsTd = document.createElement('td');
     resultsTd.textContent = allCookieStands[i].dailyTotal;
     trElement.appendChild(resultsTd);
@@ -63,14 +61,11 @@ function displayTable() {
 };
 
 displayTable();
-
 //--------form----------
-
 var subForm = document.getElementById('submission-form');
-
 var refresh = function() {
-  var oldTable=document.getElementById('tbl');
-  var container=oldTable.parentNode;
+  var oldTable = document.getElementById('tbl');
+  var container = oldTable.parentNode;
   container.removeChild(oldTable);
 };
 
@@ -84,7 +79,7 @@ var refresh = function() {
     var minimum = event.target.min.value;
     var maximum = event.target.max.value;
     var cookies = event.target.avgCookie.value;
-    var addLocation = new CookieStand(place,minimum,maximum,cookies);
+    var addLocation = new CookieStand(place, minimum, maximum, cookies);
 
     event.target.storeLoc.value = null;
     event.target.min.value = null;
